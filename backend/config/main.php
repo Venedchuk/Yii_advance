e1b7ff5ae271f5e4644f809d51f8562ab37f722c
METEOR\andrii <sneyp67@gmail.com>
2016-11-22 21:49:49 +0200

    kartik-v,admin LTE

--- backend/config/main.php
+++ backend/config/main.php
@@ -6,60 +6,50 @@ $params = array_merge(
     require(__DIR__ . '/params-local.php')
 );
 
-return [
-    'id' => 'app-backend',
-    'basePath' => dirname(__DIR__),
-    'controllerNamespace' => 'backend\controllers',
-    'bootstrap' => ['log'],
-    'modules' => [],
-    'components' => [
-        'request' => [
-            'csrfParam' => '_csrf-backend',
-        ],
-		'assetManager' => [
-			'bundles' => [
-				'dmstr\web\AdminLteAsset' => [
-					'skin' => 'skin-black',
-				],
+	return [
+		'homeUrl' => '/admin',
+		'id' => 'app-frontend',
+		'basePath' => dirname(__DIR__),
+		'bootstrap' => ['log'],
+		'controllerNamespace' => 'backend\controllers',
+		'components' => [
+			'request' => [
+				'baseUrl' => '/admin',
 			],
-		],
-		'view' => [
+			'view' => [
 			'theme' => [
 				'pathMap' => [
-					'@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/phundament/app'
+					'@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
 				],
 			],
 		],
+			'user' => [
+				'identityClass' => 'common\models\User',
+				'enableAutoLogin' => true,
+				'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+			],
+			'session' => [
+				// this is the name of the session cookie used for login on the frontend
+				'name' => 'advanced-frontend',
+			],
+			'log' => [
+				'traceLevel' => YII_DEBUG ? 3 : 0,
+				'targets' => [
+					[
+						'class' => 'yii\log\FileTarget',
+						'levels' => ['error', 'warning'],
+					],
+				],
+			],
+			'errorHandler' => [
+				'errorAction' => 'site/error',
+			],
 
-        'user' => [
-            'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
-        ],
-        'session' => [
-            // this is the name of the session cookie used for login on the backend
-            'name' => 'advanced-backend',
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
-    ],
-    'params' => $params,
-];
+			'urlManager' => [
+				'enablePrettyUrl' => true,
+				'showScriptName' => false,
+			],
+
+		],
+		'params' => $params,
+	];
